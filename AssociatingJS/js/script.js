8586322d8f067bafc282242375c3881b86d35f39
LaurieJS <LaurieJS@fullsail.edu>
2014-01-10 17:38:11 -0800

    Orders of Operations

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -44,8 +44,23 @@ var height = 5;
 var area = width * height /2;
 console.log(area);
 */
+/*
 var remainder = 32 % 2;
 console.log(remainder);
+*/
+var quiz1 = 87;
+var quiz2 = 100;
+var quiz3 = 60;
+var quiz4 = 80;
+
+var average = (quiz1 + quiz2 + quiz3 + quiz4)/4;
+//console.log(average);
+var length = 7;
+var width = 6;
+var perimeter = length * 2 + width * 2;
+console.log(perimeter);
+
+
 
 
 
