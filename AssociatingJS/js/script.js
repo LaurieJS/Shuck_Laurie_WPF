55ff1b56600100387891ed5d687679ca4e3c6587
LaurieJS <LaurieJS@fullsail.edu>
2014-01-10 17:51:40 -0800

    Concatenating Strings2

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -64,10 +64,14 @@ console.log(perimeter);
 //var orangeBins = [234, 567, 883];
 //var total = orangeBins[0] + orangeBins[1] + orangeBins[2];
 //console.log(total);
-var firstName = "Laurie";
-var lastName = "Shuck";
-var fullName = firstName + " " + lastName;
-console.log(fullName);
+//var firstName = "Laurie";
+//var lastName = "Shuck";
+//var fullName = firstName + " " + lastName;
+//console.log(fullName);
+var a = "6";
+var b = "7";
+var result = a + b;
+console.log(result);
 
 
 
