03f3cca986ef1f01190fc4ed3ba97ab7177b4b13
LaurieJS <LaurieJS@fullsail.edu>
2014-01-10 20:01:43 -0800

    Slice of the pie part 2

--- AssociatingJS/Expressions_worksheet/js/script.js
+++ AssociatingJS/Expressions_worksheet/js/script.js
@@ -15,12 +15,18 @@ console.log("Sparky is " + humanYears + " human years old, which is " + sparkyAg
 
 
 //Slice of the Pie part 1
+
+//Multiply pizza * slices and divide by people
 var slices = 5.5;
 var people = 8;
 var pizzas = 7;
 
 var slicesPerson = pizzas * slices / people;
 console.log("Each person ate " + slicesPerson + " slices of pizza at the party.");
+
+//Slice of the Pie part 2
+var sparkyPizza = pizzas * slices % people;
+console.log("Sparky got " + sparkyPizza + " slices of pizza.");
 
 
 
