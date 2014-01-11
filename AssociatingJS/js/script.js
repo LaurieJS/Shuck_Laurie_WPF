910b7ab12dd2bf65c4b620b44e323809a5762f79
LaurieJS <LaurieJS@fullsail.edu>
2014-01-10 16:16:59 -0800

    Arrays

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -14,8 +14,30 @@ age = 38;
 
 var yearOfBirth = 1970;
 */
-
+/*
 var age = 38;
 var isStudent = true;
 var firstName = "Laurie";
 var phrase = 'I don\'t know \n You\'ll have to speak to the professor.';
+*/
+var muppetNames = ["Kermit", "Miss Piggy", "Gonzo", "Rizzo"];
+console.log(muppetNames);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
