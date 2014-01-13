521c7c0ec5a5744c0ca6471bd3c61445440f01eb
LaurieJS <LaurieJS@fullsail.edu>
2014-01-13 08:59:57 -0800

    Adding additional comments

--- AssociatingJS/Expressions_worksheet/js/script.js
+++ AssociatingJS/Expressions_worksheet/js/script.js
@@ -6,28 +6,37 @@ Expressions Worksheet
 
 //Dog Years
 
-//Calculate dog years
+//Calculate Sparky's age in dog years
+//Sparky's age will be given in humanYears
+//A dog year is 7 years (dogYears)
 var humanYears = 4;
 var dogYears = 7;
 
+//The variable sparkyAge is equal to human years multiplied by dog years
 var sparkyAge = humanYears * dogYears;
+//Print to the console Sparky's age in human years and dog years
 console.log("Sparky is " + humanYears + " human years old, which is " + sparkyAge + " in dog years.");
 
 
 //Slice of the Pie part 1
 
-//Multiply pizza * slices and divide by people
+//Calculate how many slices of pizza each person will receive
+//Multiply pizza * slices, and divide by the number of people
 var slices = 5.5;
 var people = 8;
 var pizzas = 7;
 
+//Multiply pizza * slices, and divide by the number of people to find the slices per person (slicesPerson variable)
 var slicesPerson = pizzas * slices / people;
+//Print to the console the amount of slices of pizza per person
 console.log("Each person ate " + slicesPerson + " slices of pizza at the party.");
 
 //Slice of the Pie part 2
 
-//Multiply pizza * slices then divide by people with the remainder going to Sparky
+//Calculate how many slices of leftover pizza will go to Sparky 
+//Multiply pizza * slices, then divide (with modulo) by people, with the remainder going to Sparky
 var sparkyPizza = pizzas * slices % people;
+//Print to the console how many slices of pizza Sparky will receive
 console.log("Sparky got " + sparkyPizza + " slices of pizza.");
 
 
