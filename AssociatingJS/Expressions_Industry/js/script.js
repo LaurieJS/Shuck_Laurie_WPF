f634b70606ce09875b3f7eb87b8619c0b97f4029
LaurieJS <LaurieJS@fullsail.edu>
2014-01-13 18:18:18 -0800

    margin desired comment

--- AssociatingJS/Expressions_Industry/js/script.js
+++ AssociatingJS/Expressions_Industry/js/script.js
@@ -7,5 +7,6 @@
 /*This calculation will determine the width of columns on a webpage. We will need the total width of the page (px), the length of the left or right margin (px), and how many columns you would like your page to have.
  */
 
-//First we need to determine the total width of the page
+//First, we need to determine the total width of the page
 var totalWidth = prompt("What is the total width of your webpage?");
+//Next, we need to find out the amount of margin desired
