6b9dc6c801c6f3ffe1654f67dee81db4c89693d6
LaurieJS <LaurieJS@fullsail.edu>
2014-01-13 18:22:38 -0800

    created the variable margin

--- AssociatingJS/Expressions_Industry/js/script.js
+++ AssociatingJS/Expressions_Industry/js/script.js
@@ -10,3 +10,4 @@
 //First, we need to determine the total width of the page
 var totalWidth = prompt("What is the total width of your webpage?");
 //Next, we need to find out the amount of margin desired
+var margin = prompt("Indicate the margin amount that you would like on either side of the page--in pixels:");
