4c65928cd2563d540e61a760c6e964f29efcb7bf
LaurieJS <LaurieJS@fullsail.edu>
2014-01-14 11:37:18 -0800

    Adding first description

--- AssociatingJS/Expressions_Personal/js/script.js
+++ AssociatingJS/Expressions_Personal/js/script.js
@@ -8,4 +8,6 @@
 
 //When I go to dinner with friends, we often split the bill evenly after we decide on the tip amount. This caluculator will determine the cost of each diner's portion of the bill after the gratuity has been added.
 
+//We will begin by finding out the cost of the bill
+
 
