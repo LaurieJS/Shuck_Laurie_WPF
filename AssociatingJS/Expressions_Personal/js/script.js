e0329e49f798ea9ee9e683076e1802d52817ffbd
LaurieJS <LaurieJS@fullsail.edu>
2014-01-14 15:53:20 -0800

    I have made many changes because my code was not working.
    It kept adding the numbers together as strings, so I subtracted 0 from
    the prompts to force the answers to become numbers instead of strings.
    I will research this further.

--- AssociatingJS/Expressions_Personal/js/script.js
+++ AssociatingJS/Expressions_Personal/js/script.js
@@ -9,18 +9,20 @@
 //When I go to dinner with friends, we often split the bill evenly after we decide on the tip amount. This caluculator will determine the cost of each diner's portion of the bill after the gratuity has been added.
 
 //We will begin by finding out the cost of the bill.
-var bill = prompt("What is the total cost of your bill?");
+var bill = prompt("What is the total cost of your bill?") - 0;
 
 //Next, we will find out how many people are dining.
-var diners = prompt("How many diners?");
+var diners = prompt("How many diners?") - 0;
 
 //Then, we will find out the percentage of tip that they would like to leave.
-var tip = prompt("How much of a tip would you like to leave? \nExample: 20 indicates 20%.");
+var tip = prompt("How much of a tip would you like to leave? \nExample: 20 equals 20%.") - 0;
 
-//Here we will multiply the bill amount by the tax percentage
-var billCalc = bill * (tip/100);
+//Here we will begin to calculate the bill
+var tipCalc = tip /= 100;
+var billCalc = bill * tipCalc;
+var finalCost = (bill + billCalc) / diners;
 
-alert(billCalc);
+alert(finalCost);
 
 
 
