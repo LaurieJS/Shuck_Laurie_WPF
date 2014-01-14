77b79ce4442266e40761d59fd60fab03be839961
LaurieJS <LaurieJS@fullsail.edu>
2014-01-14 11:44:32 -0800

    Forgot to add the prompt info for the bill variable

--- AssociatingJS/Expressions_Personal/js/script.js
+++ AssociatingJS/Expressions_Personal/js/script.js
@@ -9,7 +9,8 @@
 //When I go to dinner with friends, we often split the bill evenly after we decide on the tip amount. This caluculator will determine the cost of each diner's portion of the bill after the gratuity has been added.
 
 //We will begin by finding out the cost of the bill.
-var bill = 200;
+var bill = prompt("What is the total cost of your bill?");
 
 //Next, we will find out how many people are dining.
+var diners = 
 
