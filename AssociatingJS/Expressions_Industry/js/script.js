522caf54a1c4aae9610a73aa11d28c0f1ec30f32
LaurieJS <LaurieJS@fullsail.edu>
2014-01-13 17:45:20 -0800

    Expressions Industry--question

--- AssociatingJS/Expressions_Industry/js/script.js
+++ AssociatingJS/Expressions_Industry/js/script.js
@@ -3,3 +3,6 @@
     January 16, 2014
     Expressions Industry Assignment
  */
+
+/*This calculation will determine the width of columns on a webpage. We will need the total width of the page (px), the length of the left or right margin (px), and how many columns you would like your page to have.
+ */
