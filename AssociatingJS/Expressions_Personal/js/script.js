4f5fab74964eb9804045ff4cfd9994f004d24a51
LaurieJS <LaurieJS@fullsail.edu>
2014-01-14 16:02:32 -0800

    Comments for finalCost

--- AssociatingJS/Expressions_Personal/js/script.js
+++ AssociatingJS/Expressions_Personal/js/script.js
@@ -14,13 +14,16 @@ var bill = prompt("What is the total cost of your bill?") - 0;
 //Next, we will find out how many people are dining.
 var diners = prompt("How many diners?") - 0;
 
-//Then, we will find out the percentage of tip that they would like to leave.
+//Then we will find out the percentage of tip that they would like to leave.
 var tip = prompt("How much of a tip would you like to leave? \nExample: 20 equals 20%.") - 0;
 
 //Here we will begin by converting the tip into a percentage.
 var tipCalc = tip /= 100;
 
+//On this line, we will multiply the bill by the tip,which is divided by 100.
 var billCalc = bill * tipCalc;
+
+/*Then we will add the bill to the billCalc, and divide that by the number of diners.*/
 var finalCost = (bill + billCalc) / diners;
 
 alert(finalCost);
