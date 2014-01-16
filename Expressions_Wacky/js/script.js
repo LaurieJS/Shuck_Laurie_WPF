d5ba0dffafe98392ddd87cdc6e9ac7b89043e4f9
LaurieJS <LaurieJS@fullsail.edu>
2014-01-15 19:50:17 -0800

    Removed some prompts and elements because there were too many.

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -8,12 +8,12 @@
  
 //This is the array that will hold the customer's information
  var shoeOrder = [];
-		 shoeOrder[0] = prompt("What is your first name?");
-		 shoeOrder[1] = prompt("What is your last name?");
-		 shoeOrder[2] = prompt("Which unicorn farm shall we deliver the shoes?");
-		 shoeOrder[3] = prompt("Enter the number of unicorns that you are purchasing shoes for:");
-		 shoeOrder[5] = prompt("Enter the number of horses that you are purchasing shoes for:");
-		 shoeOrder[6] = prompt("Are any of the animals missing feet? If yes, enter the number of feet that are missing. If not, enter 0.");
+		 shoeOrder[0] = prompt("Which unicorn farm shall we deliver the shoes?");
+		 shoeOrder[1] = prompt("Enter the number of unicorns that you are purchasing shoes for:");
+		 shoeOrder[2] = prompt("Enter the number of horses that you are purchasing shoes for:");
+		 shoeOrder[3] = prompt("Are any of the animals missing feet? If yes, enter the number of feet that are missing. If not, enter 0.");
 		 
-	var shoeCalc = shoeOrder[3]	* 4;
-	alert(shoeCalc);
+	var unicornCalc = shoeOrder[1]	* 4;
+	
+	var horseCalc = shoeOrder[2] * 4;
+	alert(horseCalc);
