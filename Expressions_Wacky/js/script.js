44b8330926b91bfa282ba62ddbd295a0da27cecb
LaurieJS <LaurieJS@fullsail.edu>
2014-01-15 18:53:18 -0800

    Changed the comments for wacky opening

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -4,8 +4,8 @@
     Expressions Wacky Assignment
  */
  
- //Starting my wacky 
+ /*You placed an order for unicorns and horses, and now you need to purchase shoes for them. This calculation will determine how many shoes are being ordered by a certain customer*/
  
  var myArray = [];
- 	   myArray[0] = "";
+		 myArray[0] = prompt("");
 		 myArray[1] = "";
