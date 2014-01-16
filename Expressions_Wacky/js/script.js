37683cc3dcc77aa35b0938bd130e242519837eab
LaurieJS <LaurieJS@fullsail.edu>
2014-01-15 19:59:09 -0800

    Changed it again. Too much info that is not required

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -4,18 +4,19 @@
     Expressions Wacky Assignment
  */
  
- /*You placed an order for unicorns and horses, and now you need to purchase shoes for them. This calculation will determine how many shoes are being ordered by a local customer*/
+ /*You placed an order for unicorns and horses, and now you need to purchase shoes for them. This calculation will determine how many shoes need to be ordered.*/
  
 //This is the array that will hold the customer's information
  var shoeOrder = [];
-		 shoeOrder[0] = prompt("Which unicorn farm shall we deliver the shoes?");
-		 shoeOrder[1] = prompt("Enter the number of unicorns that you are purchasing shoes for:");
-		 shoeOrder[2] = prompt("Enter the number of horses that you are purchasing shoes for:");
-		 shoeOrder[3] = prompt("Are any of the animals missing feet? If yes, enter the number of feet that are missing. If not, enter 0.");
+		 shoeOrder[0] = prompt("Enter the number of unicorns that you are purchasing shoes for:");
+		 shoeOrder[1] = prompt("Enter the number of horses that you are purchasing shoes for:");
+		 shoeOrder[2] = prompt("Are any of the animals missing feet? If yes, enter the number of feet that are missing. If not, enter 0.");
 		 
-	var unicornCalc = shoeOrder[1]	* 4;
+	var unicornCalc = shoeOrder[0]	* 4;
 	
-	var horseCalc = shoeOrder[2] * 4;
+	var horseCalc = shoeOrder[1] * 4;
 	
-	var shoeCalc = unicornCalc + horseCalc - shoeOrder[3];
+	var shoeCalc = unicornCalc + horseCalc - shoeOrder[2];
+	
+	console.log
 	alert(shoeCalc);
