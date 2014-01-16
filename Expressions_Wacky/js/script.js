80933ca1c034a5ffd8208e32ba5bb2290563e7d9
LaurieJS <LaurieJS@fullsail.edu>
2014-01-15 20:26:41 -0800

    Writing comments for the array

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -4,10 +4,12 @@
     Expressions Wacky Assignment
  */
  
- /*You placed an order for unicorns and horses, and now you need to purchase shoes for them. This calculation will determine how many shoes need to be ordered.*/
+ /*You placed an order for unicorns and horses and now you need to purchase shoes for them. This calculation will determine how many shoes need to be ordered.*/
  
-//This is the array that will hold the customer's information
+//This is the array that holds three prompts, which asks the user information about their shoe buying needs.
  var shoeOrder = [];
+ 
+ //This array object finds out how many unicorns need shoes
 		 shoeOrder[0] = prompt("Enter the number of unicorns that you are purchasing shoes for:");
 		 shoeOrder[1] = prompt("Enter the number of horses that you are purchasing shoes for:");
 		 shoeOrder[2] = prompt("Are any of the animals missing feet? If yes, enter the number of feet that are missing. If not, enter 0.");
