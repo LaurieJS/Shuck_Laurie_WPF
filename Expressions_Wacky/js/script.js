c14f1d3445009b0f03209813f679f6a19a54e260
LaurieJS <LaurieJS@fullsail.edu>
2014-01-15 20:45:22 -0800

    Added the comment for the shoeCalc

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -2,30 +2,32 @@
     Laurie Shuck
     January 16, 2014
     Expressions Wacky Assignment
- */
+*/
  
- /*You placed an order for unicorns and horses, and now you need to purchase shoes for them. This calculation will determine how many shoes you will need to order.*/
+/*You placed an order for unicorns and horses, and now you need to purchase shoes for them. This calculation will determine how many shoes you will need to order.*/
  
 //This is the array that holds three prompts, which asks the user information about their shoe buying needs.
- var shoeOrder = [];
+var shoeOrder = [];
  
- 		 //This array object finds out how many unicorns need shoes
-		 shoeOrder[0] = prompt("Enter the number of unicorns that you are purchasing shoes for:");
+ 		//This array object finds out how many unicorns need shoes
+		shoeOrder[0] = prompt("Enter the number of unicorns that you are purchasing shoes for:");
 		 
-		 //This array object finds out how many horses need shoes 
-		 shoeOrder[1] = prompt("Enter the number of horses that you are purchasing shoes for:");
+		//This array object finds out how many horses need shoes 
+		shoeOrder[1] = prompt("Enter the number of horses that you are purchasing shoes for:");
 		 
-		 //This array object finds out if any of the animals have missing feet
-		 shoeOrder[2] = prompt("Are any of the animals missing feet? If yes, enter the number of feet that are missing. If not, enter 0.");
+		//This array object finds out if any of the animals have missing feet
+		shoeOrder[2] = prompt("Are any of the animals missing feet? If yes, enter the number of feet that are missing. If not, enter 0.");
 		 
-	//This variable multiplies the number of unicorns by the number of feet unicorns have	 
-	var unicornCalc = shoeOrder[0]	* 4;
+//This variable multiplies the number of unicorns by the number of feet unicorns have	 
+var unicornCalc = shoeOrder[0]	* 4;
 	
-	//This variable multiplies the number of horses by the number of feet that horses have
-	var horseCalc = shoeOrder[1] * 4;
+//This variable multiplies the number of horses by the number of feet that horses have
+var horseCalc = shoeOrder[1] * 4;
 	
-	var shoeCalc = unicornCalc + horseCalc - shoeOrder[2];
+//This variable adds the number of unicorn feet and horse feet, and subtracts any missing feet
+var shoeCalc = unicornCalc + horseCalc - shoeOrder[2];
 	
-	console.log("For " + shoeOrder[0] + " unicorns and " + shoeOrder[1] + " horses, which are missing " + shoeOrder[2] + " feet, you will need to purchase " + shoeCalc + " shoes.");
+//This will print the answer to the console	
+console.log("For " + shoeOrder[0] + " unicorns and " + shoeOrder[1] + " horses, which are missing " + shoeOrder[2] + " feet, you will need to purchase " + shoeCalc + " shoes.");
 	
-	alert("For " + shoeOrder[0] + " unicorns and " + shoeOrder[1] + " horses, which are missing " + shoeOrder[2] + " feet, you will need to purchase " + shoeCalc + " shoes.");
+alert("For " + shoeOrder[0] + " unicorns and " + shoeOrder[1] + " horses, which are missing " + shoeOrder[2] + " feet, you will need to purchase " + shoeCalc + " shoes.");
