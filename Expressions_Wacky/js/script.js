8b34bfc5bbc0de6d64ad00003a608eab9bf433b5
LaurieJS <LaurieJS@fullsail.edu>
2014-01-15 18:58:27 -0800

    Added the first prompt

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -6,6 +6,7 @@
  
  /*You placed an order for unicorns and horses, and now you need to purchase shoes for them. This calculation will determine how many shoes are being ordered by a certain customer*/
  
+//This is the array that will hold the customer's information
  var myArray = [];
-		 myArray[0] = prompt("");
+		 myArray[0] = prompt("What is your first name?");
 		 myArray[1] = "";
