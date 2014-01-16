b2cca8d4282a164f97d430b2fc0a24849045d78c
LaurieJS <LaurieJS@fullsail.edu>
2014-01-15 19:02:32 -0800

    Added the second prompt

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -4,9 +4,9 @@
     Expressions Wacky Assignment
  */
  
- /*You placed an order for unicorns and horses, and now you need to purchase shoes for them. This calculation will determine how many shoes are being ordered by a certain customer*/
+ /*You placed an order for unicorns and horses, and now you need to purchase shoes for them. This calculation will determine how many shoes are being ordered by a local customer*/
  
 //This is the array that will hold the customer's information
- var myArray = [];
-		 myArray[0] = prompt("What is your first name?");
-		 myArray[1] = "";
+ var shoeOrder = [];
+		 shoeOrder[0] = prompt("What is your first name?");
+		 shoeOrder[1] = prompt("What is your last name?";
