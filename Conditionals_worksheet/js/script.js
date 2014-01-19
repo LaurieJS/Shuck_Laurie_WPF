d5e08804debb5e4499d388e93950bf7f81e99447
LaurieJS <LaurieJS@fullsail.edu>
2014-01-18 16:32:55 -0800

    Gas efficient comment

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -24,4 +24,6 @@ if(weight >= 250){
 
 //Last Chance for Gas
 
-//This will determine if you have enough gas to make it to the next gas station
+//This code will determine if you have enough gas to make it to the next gas station
+
+//The variable of your car's gas efficiency
