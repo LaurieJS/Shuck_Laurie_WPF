837c10097bff4c0fbada9ef534a4644488081fde
LaurieJS <LaurieJS@fullsail.edu>
2014-01-18 16:19:46 -0800

    Finished Stuff your face

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -9,11 +9,15 @@
 //This is the variable for the competitor's weight
 var weight = 280;
 
-//This if/else statement will determine if the competitor weighs 250 lbs or more
+//This if/else statement will determine if the competitor weighs 250 lbs or more, and print a result to the console
 
-//If weight is greater than or equal to 250
+//If competitor's weight is greater than or equal to 250
 if(weight >= 250){
+	
+	//Write to the console if the competitor is greater than 250 lbs
 	console.log("The competitor qualifies for the heavyweight division.");
+	
 }else{
+	//If the competitor is less that 250 lbs this will print to the console
 	console.log("The competitor needs to gain some weight!");	
 }
