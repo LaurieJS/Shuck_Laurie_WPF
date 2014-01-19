fdcfc0685b69d555bc3922393c36a7b4d227518b
LaurieJS <LaurieJS@fullsail.edu>
2014-01-18 16:02:22 -0800

    Created the if/else statement shell

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -9,4 +9,10 @@
 //This is the variable for the competitor's weight
 var weight = 280;
 
-//This if/else statement will determine if the competitor has qualified
+//This if/else statement will determine if the competitor weighs less than 250 lbs
+
+if(){
+	
+}else{
+	
+}
