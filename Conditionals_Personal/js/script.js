1137b618dd5693b2989232fe2c66de6d3f82faad
LaurieJS <LaurieJS@fullsail.edu>
2014-01-20 14:38:25 -0800

    Starting out on Personal-Initial comments

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -3,3 +3,5 @@
 	January 23, 2014
 	Conditionals Personal
 */
+
+//Your child received a gift card for Christmas to a major toy store. This code will determine how much you will owe, or have left on the card after the trip to the store.
