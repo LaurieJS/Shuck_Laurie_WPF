150eac81e3338c5800e764981f9b605ec321d8df
LaurieJS <LaurieJS@fullsail.edu>
2014-01-20 14:41:50 -0800

    Created the variable giftCard

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -5,3 +5,6 @@
 */
 
 //Your child received a gift card for Christmas to a major toy store. This code will determine how much you will owe, or have left on the card after the trip to the store.
+
+//This variable will determine the amount of the gift card
+var giftCard = prompt("What is the value of the gift card?");
