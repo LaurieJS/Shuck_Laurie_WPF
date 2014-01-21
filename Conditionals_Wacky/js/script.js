53341bb3829049b6d594b917fb41bbcc3bcdaaee
LaurieJS <LaurieJS@fullsail.edu>
2014-01-21 13:36:29 -0800

    Intro to Yeti

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -3,3 +3,5 @@
 	January 23, 2014
 	Conditionals Wacky
 */
+
+/*You are enjoying a relaxing stroll through the park and are confronted by a Yeti. He demands gum from you  and if you don't have enough you will suffer the wrath of the Yeti. This calculation will determine if you have enough gum to meet the Yeti's demands.*/
