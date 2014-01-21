22b2aa7e02f60ebe46210d1dbe26472c66e7a6d5
LaurieJS <LaurieJS@fullsail.edu>
2014-01-21 10:34:16 -0800

    Filled in the else/if section

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -7,7 +7,7 @@
 /*When creating responsive websites, pixel widths need to be converted to percentage widths for the site to be viewed correctly on different sized device screens. In this calculation, we will use the target/context = result formula to calculate the percentage of an element compared to its container, which will be turned into a percentage.*/
 
 //This variable will prompt the user to enter the width of their element in pixels
-var elementWidth = prompt("Enter the width of the element in pixels:");
+var elementWidth = prompt("Enter the width of the element in pixels:\nExample: if your element is 960 px enter 960");
 
 //This variable will prompt the user to enter the width of the element's container in pixels
 var container = prompt("Enter the width of the element's container in pixels:");
@@ -15,6 +15,6 @@ var container = prompt("Enter the width of the element's container in pixels:");
 //This else/if code will determine if the user prompts were left empty
 if(elementWidth === ""){
 	 elementWidth = prompt("Please enter the width of the element in pixels:");
-}else if(){
-	
+}else if(container === ""){
+	 container = prompt("Please enter the width of the element's container in pixels:");
 }
