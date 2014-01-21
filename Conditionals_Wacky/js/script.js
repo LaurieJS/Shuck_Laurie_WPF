67e03eb0fd0b6d9d919abfaac77f18a9321652fd
LaurieJS <LaurieJS@fullsail.edu>
2014-01-21 13:46:42 -0800

    I added the variable userGum which holds a prompt

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -4,4 +4,19 @@
 	Conditionals Wacky
 */
 
-/*You are enjoying a relaxing stroll through the park and are confronted by a Yeti. He demands gum from you  and if you don't have enough you will suffer the wrath of the Yeti. This calculation will determine if you have enough gum to meet the Yeti's demands.*/
+/*You are enjoying a relaxing stroll through the park and are confronted by a Yeti. He demands gum from you, and if you don't have enough, you will suffer the wrath of the Yeti. This calculation will determine if you have enough gum to meet the Yeti's demands.*/
+
+var userGum = prompt("You are enjoying a peaceful stroll through the park when you are suddenly confronted by a Yeti. He demands gum from you, and if you do not have enough gum to meet his demands, you will suffer the wrath of the Yeti.\nEnter the number of pieces of gum that he is demanding from you:");
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(userGum);
