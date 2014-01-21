cbafdee9eb38691f58aba65db0b7a573c1b7547d
LaurieJS <LaurieJS@fullsail.edu>
2014-01-21 10:53:32 -0800

    Created the comment for the calc variable

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -6,7 +6,7 @@
 
 /*When creating responsive websites, pixel widths need to be converted to percentage widths for the site to be viewed correctly on different sized device screens. In this calculation, we will use the target/context = result formula to calculate the percentage of an element compared to its container, which will be turned into a percentage.*/
 
-//This variable will prompt the user to enter the width of their element in pixels
+//This variable will prompt the user to enter the width of their element in pixels (target)
 var elementWidth = prompt("Enter the width of the element in pixels:\nExample: if your element is 960 px enter 960");
 
 //This code will determine if the elementWidth prompt was left empty
@@ -14,13 +14,16 @@ if(elementWidth === ""){
 	 elementWidth = prompt("Please enter the width of the element in pixels:");
 }
 
-//This variable will prompt the user to enter the width of the element's container in pixels
+//This variable will prompt the user to enter the width of the element's container in pixels (context)
 var container = prompt("Enter the width of the element's container in pixels:");
 
 //This code will determine if the container prompt was left empty
 if(container === ""){
 	 container = prompt("Please enter the width of the element's container in pixels:");
 }
+
+//This variable will make the following calculation: element divided by container (target divided by context) multiplied by 100
+
 
 console.log(elementWidth);
 console.log(container);
