4f65ac9e3ff3246a756251cf09d35489c4e9ef1a
LaurieJS <LaurieJS@fullsail.edu>
2014-01-21 10:14:16 -0800

    First variable comment

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -4,4 +4,7 @@
 	Conditionals Industry
 */
 
-/*When creating responsive websites pixel widths need to be converted to percentage widths for the site to be viewed correctly on different sized device screens. In this calculation, we will use the target/context = result formula to calculate the percentage of an element compared to its container, which will be turned into a percentage.*/
+/*When creating responsive websites, pixel widths need to be converted to percentage widths for the site to be viewed correctly on different sized device screens. In this calculation, we will use the target/context = result formula to calculate the percentage of an element compared to its container, which will be turned into a percentage.*/
+
+//This variable will prompt the user to enter the width of their element in pixels
+
