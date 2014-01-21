18a537561cac6269f55fc556c46a2e33acd91b54
LaurieJS <LaurieJS@fullsail.edu>
2014-01-21 10:15:46 -0800

    created the variable elementWidth

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -7,4 +7,5 @@
 /*When creating responsive websites, pixel widths need to be converted to percentage widths for the site to be viewed correctly on different sized device screens. In this calculation, we will use the target/context = result formula to calculate the percentage of an element compared to its container, which will be turned into a percentage.*/
 
 //This variable will prompt the user to enter the width of their element in pixels
+var elementWidth = prompt("Enter the width of the element in pixels:");
 
