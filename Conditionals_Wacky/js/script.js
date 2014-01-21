8c92ef1e1be59b8d590af1b5da7236f8a35b4721
LaurieJS <LaurieJS@fullsail.edu>
2014-01-21 13:48:43 -0800

    I set up the "if" statement outline if userGum prompt is empty

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -9,6 +9,11 @@
 var userGum = prompt("You are enjoying a peaceful stroll through the park when you are suddenly confronted by a Yeti. He demands gum from you, and if you do not have enough gum to meet his demands, you will suffer the wrath of the Yeti.\nEnter the number of pieces of gum that he is demanding from you:");
 
 
+if(){
+	
+}
+
+
 
 
 
