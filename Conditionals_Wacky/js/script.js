86890fd2b5be4dfeea5062a99bfa5a8262bf47c6
LaurieJS <LaurieJS@fullsail.edu>
2014-01-21 14:06:10 -0800

    Changed userGum to gumDemand

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -6,11 +6,13 @@
 
 /*You are enjoying a relaxing stroll through the park and are confronted by a Yeti. He demands gum from you, and if you don't have enough, you will suffer the wrath of the Yeti. This calculation will determine if you have enough gum to meet the Yeti's demands.*/
 
-var userGum = prompt("You are enjoying a peaceful stroll through the park when you are suddenly confronted by a Yeti. He demands gum from you, and if you do not have enough gum to meet his demands, you will suffer the wrath of the Yeti.\nEnter the number of pieces of gum that he is demanding from you:");
+var gumDemand = prompt("You are enjoying a peaceful stroll through the park when you are suddenly confronted by a Yeti. He demands gum from you, and if you do not have enough gum to meet his demands, you will suffer the wrath of the Yeti.\nEnter the number of pieces of gum that he is demanding from you:");
 
 //This if statement will determine if the userGum promt is empty
-if(userGum === ""){
-	 userGum = prompt("Everyone knows that Yeti demands are nerve wracking, can you please enter the number of pieces of gum the Yeti is demanding from you.");
+if(gumDemand === ""){
+	
+	 //If true, the user will be prompted to enter the gum demand again
+	 gumDemand = prompt("Everyone knows that Yeti demands are nerve wracking, can you please enter the number of pieces of gum the Yeti is demanding from you.");
 }
 
 
