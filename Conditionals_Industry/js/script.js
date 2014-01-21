913729d0e52de3fef4d1d779a3d3b80d450e1541
LaurieJS <LaurieJS@fullsail.edu>
2014-01-21 10:11:41 -0800

    The initial comment for Industry

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -3,3 +3,5 @@
 	January 23, 2014
 	Conditionals Industry
 */
+
+/*When creating responsive websites pixel widths need to be converted to percentage widths for the site to be viewed correctly on different sized device screens. In this calculation, we will use the target/context = result formula to calculate the percentage of an element compared to its container, which will be turned into a percentage.*/
