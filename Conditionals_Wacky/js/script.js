fdc32e34a5f347614f172b7698079fc7ab0d4c52
LaurieJS <LaurieJS@fullsail.edu>
2014-01-21 17:19:48 -0800

    Fixing the story and grammar

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -6,7 +6,7 @@
 
 /*You are enjoying a relaxing stroll through the park and are confronted by a Yeti. He demands gum from you, and if you don't have enough, you will suffer the wrath of the Yeti. This calculation will determine if you have enough gum to meet the Yeti's demands.*/
 
-var gumDemand = prompt("You are enjoying a peaceful stroll through the park when you are suddenly confronted by a Yeti. He demands gum from you, and if you do not have enough gum to meet his demands, you will suffer the wrath of the Yeti.\nEnter the number of pieces of gum that he is demanding from you:");
+var gumDemand = prompt("You are enjoying a peaceful stroll through the park when you are suddenly confronted by a Yeti. He demands gum from you and if you do not have enough gum to meet his demands then you will suffer the wrath of the Yeti.\nEnter the number of pieces of gum that he is demanding from you:");
 
 //This if statement will determine if the userGum prompt is empty
 if(gumDemand === ""){
@@ -34,15 +34,24 @@ if(runningSpeed === ""){
 //This variable will subtract the amount of gum that the user has from the Yeti's gum demand
 var gumCalc = userGum - gumDemand;
 
+var youLose = "Unfortunately, you do not have enough gum, and you are not a fast runner. This causes the Yeti to roar very loud directly in your face. You wish that you had enough gum because he has horrible halitosis. Sorry that you had to suffer the wrath of the Yeti. Maybe next time you will have more gum."
+
+var youEscaped = "You ran away and escaped the wrath of the Yeti...this time."
+
+var youWin = "You give the Yeti the gum that he demanded. He proceeds to make you a balloon animal from the gum to take home to your kids. He asks if you will be back next week, and you say that you will be back next week and that you will bring your kids."
+
 //This else/if statement will determine the outcome of the encounter
 
 //If the gumCalculation is less than zero, AND the user is a not a fast runner
-if(gumCalc < 0 && runningSpeed === false){
-	 console.log("Unfortunately, you do not have enough gum, and you are not a fast runner. This causes the Yeti to roar very loud directly in your face. You wish that you had enough gum because he has horrible halitosis. Sorry, you had to suffer the wrath of the Yeti. Maybe next time you will have more gum.");
-}else if(gumCalc < 0 && runningSpeed === true){
-	 console.log("You ran away and escaped the wrath of the Yeti...this time.");
+if(gumCalc < 0 && runningSpeed == "no"){
+	 alert(youLose);
+	 console.log(youLose);
+}else if(gumCalc < 0 && runningSpeed == "yes"){
+	 alert(youEscaped);
+	 console.log(youEscaped);
 }else if(gumCalc >= 0){
-	
+	 alert(youWin);
+	 console.log(youWin);
 }else{
 	
 }
