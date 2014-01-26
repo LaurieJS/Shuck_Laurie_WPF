f6fb86b9237bf0db1aa6d788c0c3bbceecb9ed1b
LaurieJS <LaurieJS@fullsail.edu>
2014-01-25 16:32:22 -0800

    Added the circumference variable and the console.log

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -12,11 +12,17 @@ var radius = 7;
 //This variable holds the value of pi
 var pi = Math.PI;
 
+//This is the variable that holds the circumference variable
+var circumference = calcCircumference(radius, pi);
 
 //This is the function that will calculate the circumference
 function calcCircumference(radius, pi){
 	return radius * 2 * pi;
 }
 
+//The extra console.logs are for my benefit to test the code
+/*
 console.log(radius);
 console.log(pi);
+*/
+console.log(circumference);
