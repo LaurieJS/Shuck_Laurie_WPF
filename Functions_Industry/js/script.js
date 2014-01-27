a4b956a82b5ad121180a944dbdc54caa54b80ff7
LaurieJS <LaurieJS@fullsail.edu>
2014-01-26 19:52:45 -0800

    Added the initial comments

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -5,3 +5,5 @@
  */
  
 //If an image on a website requires a size adjustment to fill a certain height or width, this calculation will help determine the new size of the image adjusted proportionally in pixels.
+
+//This variable will hold the original width of the image
