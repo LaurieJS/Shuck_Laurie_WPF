73b6fe87b00f36115884c6b3249a4f9947344f52
LaurieJS <LaurieJS@fullsail.edu>
2014-01-26 20:00:15 -0800

    Added the width variable

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -6,4 +6,10 @@
  
 //If an image on a website requires a size adjustment to fill a certain height or width, this calculation will help determine the new size of the image adjusted proportionally in pixels.
 
-//This variable will hold the original width of the image
+//This variable will prompt the user to enter the original width of the image
+var originalWidth = prompt("Please enter the width of the image in pixels:");
+
+
+
+
+console.log(originalWidth);
