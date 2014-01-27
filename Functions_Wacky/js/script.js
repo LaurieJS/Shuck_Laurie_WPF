86980bc64c8cdc4c01db486e11f639ea832a6a31
LaurieJS <LaurieJS@fullsail.edu>
2014-01-27 15:26:25 -0800

    Comments for the gold variable and its while statement

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -9,11 +9,14 @@
  
 /*You are watching TV when you are magically sucked into the Inner Earth. The Leprechauns that reside there inform you that there is no way for you to return home because they are unable to escape the InnerEarth's gravity. The Leprechauns offer to build you a two bedroom home for a piece of gold, and if you have any other pieces of gold they will build two additional rooms to your house. In addition, if you have any Federal Reserve Notes they will build you one outhouse per note regardless of its denomination.*/
 
-
+//This variable holds the prompt that asks the user how many pieces of gold they have
 var gold = prompt("How many pieces of gold were you wearing when you magically got sucked into the Inner Earth?");
 
+
+//While the gold prompt remains empty
 while(gold == ""){
 	
+		//This prompt will appear until the user fills in how many pieces of gold they have
 		gold = prompt("Please enter the number of pieces of gold you were wearing when you were magically sucked into the Inner Earth:");
 }
 
@@ -40,9 +43,9 @@ if(final >= 12){
 	
 	
 }else if(final < 12 && final > 0){
-		console.log(message + "You have a modest home, but you still have room to have a couple miniature giraffes as pets.");
+		console.log(message + " You have a modest home, but you still have room to have a couple miniature giraffes as pets.");
 }else{
-	  console.log(message + "You better start looking around for twigs and leaves to make shelter. Hopefully, you have something the Leprechauns want, or you are going to be a homeless Inner Earth dweller.");
+	  console.log(message + " You better start looking around for twigs and leaves to make shelter. Hopefully, you have something the Leprechauns want, or you are going to be a homeless Inner Earth dweller.");
 
 }
 
