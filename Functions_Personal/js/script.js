3e29599438c2b6e62a25a10aaa697bcdf3bebce2
LaurieJS <LaurieJS@fullsail.edu>
2014-01-27 09:28:57 -0800

    Added the cerealBox variable

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -9,3 +9,10 @@
 //My children eat an enormous amount of cereal. This calculation will determine how many bowls of cereal that a household consumes in a year.
 
 //This variable will prompt the user to enter the number of boxes of cereal that are consumed in a week, on average
+var cerealBox = prompt("Please enter the number of boxes of cereal that your household consumes per week:");
+
+
+
+
+
+console.log(cerealBox);
