d9f228d56aa745eedc7ee04f87f2ec01a6de57b9
LaurieJS <LaurieJS@fullsail.edu>
2014-01-27 12:54:53 -0800

    Starting on Functions Wacky

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -3,3 +3,8 @@
     January 30, 2014
     Functions Wacky Assignment
  */
+ 
+ 
+//Inner Earth
+ 
+/*You are watching TV when you are magically sucked into the Inner Earth. The Leprechauns that reside there inform you that there is no way for you to return home because they are unable to escape the InnerEarth's gravity. The Leprechauns offer to build you a two bedroom home for a piece of gold, and if you have any other pieces of gold they will build two additional rooms to your house. In addition, if you have any Federal Reserve Notes they will build you one outhouse per note regardless of its denomination.*/
