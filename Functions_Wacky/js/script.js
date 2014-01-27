89ec6797b215afbe058eb6d49fe52a90837ed998
LaurieJS <LaurieJS@fullsail.edu>
2014-01-27 12:56:44 -0800

    Made a gold variable

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -8,3 +8,6 @@
 //Inner Earth
  
 /*You are watching TV when you are magically sucked into the Inner Earth. The Leprechauns that reside there inform you that there is no way for you to return home because they are unable to escape the InnerEarth's gravity. The Leprechauns offer to build you a two bedroom home for a piece of gold, and if you have any other pieces of gold they will build two additional rooms to your house. In addition, if you have any Federal Reserve Notes they will build you one outhouse per note regardless of its denomination.*/
+
+
+var gold = prompt("How many pieces of gold were you wearing when you magically got sucked into the Inner Earth?");
