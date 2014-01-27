519b309abaaf71d157ebc0c03b91d7684cd47b16
LaurieJS <LaurieJS@fullsail.edu>
2014-01-26 19:50:53 -0800

    Getting started on  Functions Industry

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -3,3 +3,5 @@
     January 30, 2014
     Functions Industry Assignment
  */
+ 
+//If an image on a website requires a size adjustment to fill a certain height or width, this calculation will help determine the new size of the image adjusted proportionally in pixels.
