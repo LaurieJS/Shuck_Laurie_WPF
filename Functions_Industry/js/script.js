14601b7be9ae3b542d2f0091445487178957cc5f
LaurieJS <LaurieJS@fullsail.edu>
2014-01-30 17:27:57 -0800

    Numerous changes

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -8,27 +8,29 @@
 
 
 //This variable will prompt the user to enter the original width of the image
-var originalWidth = prompt("Please enter the width of the image in pixels:");
+var originalWidth = prompt("Please enter the original width of the image in pixels:");
 
 
 //While the originalWidth prompt is empty or 0 this will keep prompting the user to enter the width until they fill in the prompt
-while(originalWidth == "" || originalWidth == 0){
-	
-		//The user will prompted to answer this question until they fill in the prompt
-	 	originalWidth = prompt("To continue with the calculation, the width of the image is required.\nPlease enter the numerical value of the image's width:");
+while (originalWidth === "" || originalWidth === 0) {
+
+    //The user will prompted to answer this question until they fill in the prompt
+    originalWidth = prompt("To continue with the calculation, the width of the image is required.\nPlease enter the numerical value of the image's width:");
 }
+
 
 
 //This variable will prompt the user to enter the original height of the image
-var originalHeight = prompt("Please enter the height of the image in pixels:");
+var originalHeight = prompt("Please enter the original height of the image in pixels:");
 
 
-//This while loop will keep prompting the user to enter the original height of the image
-while(originalHeight == "" || originalHeight == 0){
-	
-		//The user will prompted to answer this question until they fill in the prompt
-	 	originalHeight = prompt("To continue with the calculation, the height of the image is required.\nPlease enter the numerical value of the image's height:");
+//This while loop will keep prompting the user to enter the original height of the image if the prompt was left empty or 0
+while (originalHeight === "" || originalHeight === 0) {
+
+    //The user will prompted to answer this question until they fill in the prompt
+    originalHeight = prompt("To continue with the calculation, the height of the image is required.\nPlease enter the numerical value of the image's height:");
 }
+
 
 
 //This variable will prompt the user to enter whether they have a new height or a new width
@@ -36,106 +38,81 @@ var userChoice = prompt("Will you be entering the new width that you would like 
 
 
 //This while loop will keep prompting the user to enter if they have a new height or a new width
-while(userChoice == "" || userChoice == 0){
-	
-		//The user will prompted to answer this question until they fill in the prompt
-	 	userChoice = prompt("To continue with the calculation 'width' or 'height' must be entered. Please type in your answer:");
+while (userChoice === "" || userChoice === 0) {
+
+    //The user will be prompted to answer this question until they fill in the prompt
+    userChoice = prompt("To continue with the calculation 'width' or 'height' must be entered. Please type in your answer:");
 }
-
-
-//This variable will hold the data the user enters when they choose width
-var newWidth;
-
-//This variable will hold the data the user enters when they choose height
-var newHeight;
 
 
 
 //If the user's answer is width
-if(userChoice == "width"){
-	
-		//This variable will prompt the user to enter the new width
-		newWidth = prompt("Please enter the new width that you would like the image to be:");		
+if (userChoice == "width") {
 
-//Or, if the user's answer is height		 		
-}else if(userChoice == "height"){
-	
-		//This variable will prompt the user to enter the new height
-		newHeight = prompt("Please enter the new height that you would like the image to be:");
-		
-//Or, if they answered with something other than width or height		
-}else{
-	
-		//The user will be alerted to go back and fill in height or width
-	  alert("Please go back and fill in whether you know the new height or the new width of the image. Thanks!");
-}
+    //This variable will prompt the user to enter the new width
+    var newWidth = prompt("Please enter the new width that you would like the image to be:");
 
-//While the newWidth prompt is empty or 0
-while(newWidth == "" || newWidth == 0){
-	
-		 //The user will prompted to answer the question until they fill in the prompt
-	   newWidth = prompt("Sorry, but we cannot continue until you enter the new width that you would like the image to be:");
-}
+//Or, if the user's answer is height
+} else if (userChoice == "height") {
 
-//While the newHeight prompt is empty or 0
-while(newHeight == "" || newHeight == 0){
-	
-		 //The user will prompted to answer this question until they fill in the prompt
-	   newHeight = prompt("Sorry, but we cannot continue until you enter the new width that you would like the image to be:");
+    //This variable will prompt the user to enter the new height
+    var newHeight = prompt("Please enter the new height that you would like the image to be:");
+
+//Or, if they answered with something other than width or height
+} else {
+
+    //The user will be alerted to go back and fill in height or width
+    alert("Please go back and fill in whether you know the new height or the new width of the image. Thanks!");
 }
 
 
+//While the newWidth prompt is empty or equal to 0
+while (newWidth === "" || newWidth === 0) {
 
-//This function calculates the information using the data gathered when the user's answer is width	
-function calculateWidth(originalWidth, originalHeight, newWidth){
-	
-		//This will return the originalHeight multiplied by the newWidth divided by the originalWidth
-		return originalHeight * newWidth / originalWidth;
-	
+    //The user will prompted to answer the question until they fill in the prompt
+    newWidth = prompt("Sorry, but we cannot continue until you enter the new width that you would like the image to be:");
+}
+
+//While the newHeight prompt is empty or equal to 0
+while (newHeight === "" || newHeight === 0) {
+
+    //The user will prompted to answer this question until they fill in the prompt
+    newHeight = prompt("Sorry, but we cannot continue until you enter the new width that you would like the image to be:");
 }
 
 
+//I created an anonymous function to calculate the width
+var widthTotal = function () {
+
+    //This variable will make the width calculation
+    var widthFinal = originalHeight * newWidth / originalWidth;
+
+    //This will return a message to the user when the function is called
+    return "Your original image width is " + originalWidth + " pixels, and the height is " + originalHeight + " pixels. When you change the width to " + newWidth + " pixels, the new height will be " + widthFinal + " pixels.";
+};
 
 
-//This variable will hold the answer returned from the width function
-var widthTotal = calculateWidth(originalWidth, originalHeight, newWidth);
+//I created an anonymous function to calculate the height
+var heightTotal = function () {
 
-//This variable will hold the message that will print to the console if the user chose width
-var widthMessage = "Your original image width is " + originalWidth + " pixels and the height is " + originalHeight + " pixels. When you change the width to " + newWidth + " pixels the new height will be " + widthTotal + " pixels.";
+    //This variable will multiply the original width by the new height, which is divided
+    var heightFinal = originalWidth * newHeight / originalHeight;
 
-
-
-//This function calculates the information using the data gathered when the user's answer is height
-function calculateHeight(originalWidth, newHeight, originalHeight){
-			
-			//This will return the information originalWidth multplied by newHeight divided by the originalHeight
-	 		return originalWidth * newHeight / originalHeight;
-			
-}
+    //This will return a message to the user when the function is called
+    return "Your original image width is " + originalWidth + " pixels, and the height is " + originalHeight + " pixels. When you change the height to " + newHeight + " pixels, the new width will be " + heightFinal + " pixels.";
+};
 
 
 
-
-//This variable will hold the answer returned from the height function
-var heightTotal = calculateHeight(originalWidth, newHeight, originalHeight);
-
-//This variable holds the message that will print to the console if they chose height 
-var heightMessage = "Your original image width is " + originalWidth + " pixels and the height is " + originalHeight + " pixels. When you change the height to " + newHeight + " pixels the new width will be " + heightTotal + " pixels.";
+//This ternary with alert the user with the widthTotal function message if the user entered width, or else it will use the heightTotal function message
+(userChoice == "width") ? alert(widthTotal()) : alert(heightTotal());
 
 
-//If the user's choice is width the widthMessage will be alerted, and if not the heightMessage will be alerted to the user
-(userChoice == "width") ? alert(widthMessage) : alert(heightMessage);
+//This ternary with print to the console the widthTotal function message if the user entered width, or else it will use the heightTotal function message
+(userChoice == "width") ? console.log(widthTotal()) : console.log(heightTotal());
 
 
-//If the user's choice is width the widthMessage will print to the console, and if not the heightMessage will print to the console
-(userChoice == "width") ? console.log(widthMessage) : console.log(heightMessage);
 
 
-/*
-console.log(originalWidth);
-console.log(originalHeight);
-console.log(userChoice);
-console.log(newWidth);
-console.log(newHeight);
-console.log(widthTotal);
-*/
+
+
--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -2,21 +2,22 @@
     Laurie Shuck
     January 30, 2014
     Functions Personal Assignment
- */
+*/
  
 //How Many Bowls of Cereal?
 
-//My children eat an enormous amount of cereal. This calculation will determine how many bowls of cereal that a household consumes in a year.
+//My children eat an enormous amount of cereal. This calculation will determine how many bowls of cereal that a household consumes in a certain amount of years.
+
 
 //This variable will prompt the user to enter the number of boxes of cereal that are consumed in a week, on average
 var cerealBox = prompt("Please enter the number of boxes of cereal that your household consumes per week:");
 
 
 //While the cerealBox prompt is empty the user will be prompted to enter the number of boxes of cereal
-while(cerealBox == ""){
-	
-			//This will collect the data and place the info in the cerealBox variable
-	   cerealBox = prompt("Please enter the number of boxes or cereal that your household consumes per week:");
+while (cerealBox === "") {
+
+    //This will collect the data and place the info in the cerealBox variable
+    cerealBox = prompt("The number of boxes or cereal that your household consumes per week is required information:");
 }
 
 
@@ -25,52 +26,44 @@ var bowlsPerBox = prompt("On average, how many bowls of cereal does each box mak
 
 
 //While the bowlsPerBox prompt is empty the user will be prompted to enter the number of bowls of cereal
-while(bowlsPerBox == ""){
-	
-		//This will prompt the user to answer the number bowls of cereal per box
-		bowlsPerBox = prompt("To continue with the calculation, the number of bowls each box makes is required:");
-	
+while (bowlsPerBox === "") {
+
+    //This will prompt the user to answer the number bowls of cereal per box
+    bowlsPerBox = prompt("To continue with the calculation, the number of bowls of cereal that each box makes is required:");
+
 }
 
 
-//This variable holds the number of weeks in a year
-var weeksYear = 52;
+//This variable prompts the user to answer how many years they would like to calculate
+var userYear = prompt("How many years of cereal consuption would you like to calculate?");
 
 
+//While the userYear prompt is empty the user will be prompted to enter the number of years they would like to calculate
+while (userYear === "") {
 
+    //This will prompt the user to answer the number of years that they would like to calculate
+    userYear = prompt("To continue with the calculation, the number of years of that you would like to calculate are required:");
 
-//This function will calculate the amount of bowls of cereal eaten in a year
-function calculateCereal(cerealBox, bowlsPerBox, weeksYear){
-	
-		//This return statement multiplies the boxes of cereal by the amount of bowls in each box, multiplied by the number of weeks in a year
-		return cerealBox * bowlsPerBox * weeksYear;
 }
 
 
+//This function will calculate the amount of bowls of cereal eaten in a certain amount of years
+function calculateCereal(boxes, bowls, years) {
 
-//This variable collects the information from the function calculation
-var cerealTotal = calculateCereal(cerealBox, bowlsPerBox, weeksYear);
-
-//This variable holds the message that will print to the console adn be alerted to the user
-var cerealMessage = "In one week, your household eats " + cerealBox + " boxes of cereal, and each box makes " + bowlsPerBox + " bowls of cereal. In one year, your household will eat " + cerealTotal + " bowls of cereal.";
-
-//This will alert the user of their cereal consumption
-alert(cerealMessage);
-
-//This will print to the console the user's cereal consumption
-console.log(cerealMessage);
-
-
-/*
-console.log(cerealBox);
-console.log(bowlsPerBox);
-console.log(weeksYear);
-console.log(cerealTotal);
-console.log(cerealMessage);
-*/
+    //This variable holds the answer to the number of boxes of cereal mutiplied by the amount of bowls in each box, and then it is multiplied by the number of years, which is multiplied by 52
+    var total = boxes * bowls * (years * 52);
+		
+		//This message will be alerted to the user
+		alert("In one week, your household eats " + boxes + " boxes of cereal, and each box makes " + bowls + " bowls of cereal. In " + years + " years, your household will eat " + total + " bowls of cereal.");
+		
+		 //This message will appear in the console
+    console.log("In one week, your household eats " + boxes + " boxes of cereal, and each box makes " + bowls + " bowls of cereal. In " + years + " years, your household will eat " + total + " bowls of cereal.");
+		
+}//End of the calculateCereal function
 
 
 
-
+//This will call the calculateCereal function using the global variable's prompts as arguments
+calculateCereal(cerealBox, bowlsPerBox, userYear);
 
 
